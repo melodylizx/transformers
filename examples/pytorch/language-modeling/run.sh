ef6ee83557fd7e62dfc3a7fdb16e4afbfba66c87
Melody Li <104742277+melodylizx@users.noreply.github.com>
2024-07-11 01:51:25 -0400

    Update run.sh

--- examples/pytorch/language-modeling/run.sh
+++ examples/pytorch/language-modeling/run.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 #SBATCH --account=rrg-tyrell-ab
-#SBATCH --mem=32G       
-#SBATCH --gres=gpu:2                # Ask for 1 GPU
+#SBATCH --mem=60G       
+#SBATCH --gres=gpu:4              
 ######SBATCH --time=48:00:00
 #SBATCH --time=120:00:00
-#SBATCH --cpus-per-gpu=2
-#SBATCH --output=sbatch_out/run1.%A.%a.out
-#SBATCH --error=sbatch_err/run1.%A.%a.err
-#SBATCH --job-name=run1
+#SBATCH --cpus-per-gpu=4
+#SBATCH -o /scratch/melody/slurm-%j.out
 
 
 source llm/bin/activate
-#cd projects/def-tyrell/melodyli/transformers/examples/pytorch/language-modeling/
+module load arrow
+cd projects/def-tyrell/melodyli/transformers/examples/pytorch/language-modeling/
+
 
 torchrun --nproc_per_node=4 run_clm.py \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
     --per_device_train_batch_size 24 \
     --per_device_eval_batch_size 24 \
+    --ddp_timeout 18000 \
     --do_train \
     --do_eval \
-    --output_dir /home/melodyli/scratch/gpt2_ckpts_20000 \
+    --output_dir /home/melodyli/scratch/ \
     --model_type gpt2 \
     --tokenizer_name tokenizer.json \
-    --num_train_epochs 20000 \
+    --num_train_epochs 5 \
     --fp16 \
     --learning_rate 6e-4 \
     --lr_scheduler_type cosine \
